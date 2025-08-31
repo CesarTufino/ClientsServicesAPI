4070a4f64110862deb11dee20607a4789b4a78fa
CesarTufino <cesar.tufino@epn.edu.ec>
2025-08-31 13:30:41 -0500

    chore: add docker compose file and connection string

--- ClientsServicesAPI/Program.cs
+++ ClientsServicesAPI/Program.cs
@@ -1,6 +1,12 @@
+using ClientsServicesAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add DbContext with SQL Server provider
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
